b6c7eb6b1c6d2ac92ccec47d39edf221ac800e90
Priyans4475 <117758759+Priyans4475@users.noreply.github.com>
2025-09-05 23:31:18 +0530

    Create P011.cpp

---
+++ P011.cpp
@@ -0,0 +1,59 @@
+class DisjointSet {
+public:
+    vector<int> rank, parent, size;
+    DisjointSet(int n) {
+        rank.resize(n + 1, 0);
+        parent.resize(n + 1);
+        size.resize(n + 1);
+        for (int i = 0; i <= n; i++) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
+
+    int findUPar(int node) {
+        if (node == parent[node])
+            return node;
+        return parent[node] = findUPar(parent[node]);
+    }
+
+    
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findUPar(u);
+        int ulp_v = findUPar(v);
+        if (ulp_u == ulp_v) return;
+        if (size[ulp_u] < size[ulp_v]) {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+        else {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& edge) {
+         
+        int total_stones=edge.size();
+        cout<<total_stones;
+        DisjointSet ds(total_stones);
+        for(int i = 0; i<total_stones; i++) {
+            for(int j = i+1; j<total_stones; j++) {
+                if(edge[i][0] == edge[j][0] || edge[i][1] == edge[j][1])
+                   ds.unionBySize(i,j);
+            }
+        }
+        int components = 0;
+        for (int i = 0; i < total_stones; i++) {
+            if (ds.parent[i] == i) components++;
+        }
+        cout<<components;
+        
+       return total_stones-components;
+       
+    }
+};
